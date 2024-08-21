46f503e3fa17d68b1a31aaaf5a742183dddfc866
Fatimah <Nyzak1@outlook.sa>
2024-08-21 23:46:23 +0300

    Add Color palette

---
+++ lib/style.dart
@@ -0,0 +1,19 @@
+import "package:flutter/material.dart";
+
+//Color Palette for the app
+
+//Primary Colors:
+const FieryOrange = Color(0xFFFF6B35);//Represents the flame and energy.
+const DarkCharcoal  = Color(0xFF1C1C1C);// For backgrounds, providing contrast.
+const AshGray = Color(0xFFA8A8A8);//Secondary elements like icons, borders.
+
+//Accent Colors:
+const BrightYellow = Color(0xFFFFC300);//Highlights and buttons.
+const WarmRed = Color(0xFFE63946);//Error states or notifications.
+
+//Neutral Colors:
+const OffWhite  = Color(0xFFF8F9FA);//Text, input fields, backgrounds.
+const LightGray = Color(0xFFDEE2E6);//For dividers, subtle text.
+
+//Text Styles
+//logo //icons //...etc
